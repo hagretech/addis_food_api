3e42d02f4b53fc3671790046a93ab4cae04b685d
kirubel demelah <kiztopia.2020@gmail.com>
2021-02-20 13:21:03 -0500

    adding company name in food model

--- models/food_model.js
+++ models/food_model.js
@@ -20,6 +20,14 @@ const foodSchema = mongoose.Schema({
     company_id: {
         type: String,
         required: true,
+    },
+    company_name: {
+        type: String,
+        required: true
+    },
+    company_address: {
+        type: String,
+        required: true
     }
 });
 
--- routes/food.js
+++ routes/food.js
@@ -10,8 +10,14 @@ router.post('/', async(req, res) => {
     const { name, price, tag, catagory, company_id, } = req.body;
     console.log("your info is made ", name, catagory, )
     try {
-        const food = await Food.create({ name, price, catagory, tag, company_id });
-        res.status(201).json({ food });
+        const company = await Company.findById(company_id)
+            .then(async company => {
+                const company_name = company.name;
+                const company_address = company.address;
+                const food = await Food.create({ name, price, catagory, tag, company_id, company_address, company_name });
+                res.status(201).json({ food });
+            })
+
     } catch (err) {
         //const errors = handleErrors(err);
         res.status(400).json({ err });
@@ -27,7 +33,7 @@ router.get('/company/:company_id', (req, res, ) => {
     var company_id = req.params.company_id
     comp = Company.findById(company_id)
         .then(company => {
-            food = Food.find({ company_id })
+            food = Food.find()
                 .then(foods => {
                     res.json({ company, foods })
                 })
